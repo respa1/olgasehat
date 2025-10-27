c42020fbb69b9ec0792862db0642c821a25e521d
rendra7-rpl <pratamarendra904@gmail.com>
2025-10-27 11:35:20 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,7 @@ Route::get('/venueuser_detail', fn() => view('user.venueuser_detail'));
 Route::get('/communityuser', fn() => view('user.communityuser'));
 Route::get('/communityuser_detail', fn() => view('user.communityuser_detail'));
 Route::get('/membership-user-detail', fn() => view('user.membershipuser_detail'));
+Route::get('/healthyuser', fn() => view('user.healthyuser'));
 
 // ======================================================
 // PEMILIK / MITRA ROUTES
